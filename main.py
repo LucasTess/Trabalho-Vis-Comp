b1951ff850d5452b73a45297dce6f2ac9385ca2e
LucasTess <lucasrtessari@gmail.com>
2024-12-01 20:46:44 -0300

    Começando  modularização pra entender o código

---
+++ main.py
@@ -0,0 +1,8 @@
+from PyQt5.QtWidgets import QApplication
+import sys
+from main_window import MainWindow
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())

1f269ad91be178fc025e459bebb499dd4eac0eca
joaogabriel <joaogabriel.sc99@gmail.com>
2024-12-09 05:42:06 -0300

    feat: fix docstring

--- utils/draw_arrows.py
+++ utils/draw_arrows.py
@@ -7,16 +7,16 @@ def draw_arrows(
         axis: 'matplotlib.axes.Axes',
         length: float = 25
     ) -> 'matplotlib.axes.Axes':
-    """Desenha vetores que representam os eixos a partir de um ponto específico no espaço 3D.
+    """Draws vectors representing axes from a specific point in 3D space.
 
     Args:
-        point (np.ndarray): Coordenadas do ponto de origem dos vetores (shape (3,)).
-        base (np.ndarray): Matriz 3x3 representando os vetores base (colunas são os vetores).
-        axis (matplotlib.axes.Axes): Objeto do matplotlib para desenhar os vetores.
-        length (float, optional): Comprimento dos vetores. Padrão é 25.
+        point (np.ndarray): Coordinates of the origin point of the vectors (shape (3,)).
+        base (np.ndarray): 3x3 matrix representing the base vectors (columns are the vectors).
+        axis (matplotlib.axes.Axes): Matplotlib object to draw the vectors.
+        length (float, optional): Length of the vectors. Default is 25.
 
     Returns:
-        matplotlib.axes.Axes: O eixo com os vetores adicionados.
+        matplotlib.axes.Axes: The axis with the vectors added.
     """
 
     axis.quiver(point[0],point[1],point[2],base[0,0],base[1,0],base[2,0],color='red',pivot='tail',  length=length)
@@ -24,7 +24,3 @@ def draw_arrows(
     axis.quiver(point[0],point[1],point[2],base[0,2],base[1,2],base[2,2],color='blue',pivot='tail',  length=length)
 
     return axis
-
-
-# np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(float(x))})
-# np.set_printoptions(precision=3,suppress=True)
